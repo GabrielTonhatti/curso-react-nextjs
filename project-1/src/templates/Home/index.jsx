b0d7dbe021ef73054737d2846af52775942b8e88
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-10-05 18:00:23 -0300

    Props e organização dos componentes - Parte 3

--- project-1/src/templates/Home/index.jsx
+++ project-1/src/templates/Home/index.jsx
@@ -9,7 +9,8 @@ export class Home extends Component {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 10,
+    postsPerPage: 2,
+    searchValue: "",
   };
 
   async componentDidMount() {
@@ -35,19 +36,47 @@ export class Home extends Component {
     this.setState({ posts, page: nextPage });
   };
 
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({ searchValue: value });
+  };
+
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue
+      ? allPosts.filter((post) =>
+          post.title.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      : posts;
 
     return (
       <section className="container">
-        <Posts posts={posts} />
+        {!!searchValue && (
+          <>
+            <h1>Search value: {searchValue}</h1>
+          </>
+        )}
+
+        <br />
+        <br />
+        <input type="search" value={searchValue} onChange={this.handleChange} />
+        <br />
+        <br />
+        <br />
+
+        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+        {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
+
         <div className="button-container">
-          <Button
-            text="Load more posts"
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
+          {!searchValue && (
+            <Button
+              text="Load more posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
