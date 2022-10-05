5cb45e9faacb2d05416c2d63001d8e8d183a9229
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-10-05 10:01:01 -0300

    State com arrays e objetos

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,48 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
     state = {
-        name: "Gabriel Tonhatti",
-        counter: 0,
-    };
-
-    handlePClick = () => {
-        this.setState({ name: "Júnior" });
-    };
-
-    handleAClick = (event) => {
-        event.preventDefault();
-
-        const { counter } = this.state;
-        this.setState({ counter: counter + 1 });
+        posts: [
+            {
+                id: 1,
+                title: "O título 1",
+                body: "O corpo 1",
+            },
+            {
+                id: 2,
+                title: "O título 2",
+                body: "O corpo 2",
+            },
+            {
+                id: 3,
+                title: "O título 3",
+                body: "O corpo 3",
+            },
+        ],
     };
 
     render() {
-        const { name, counter } = this.state;
+        const { posts } = this.state;
 
         return (
-            <>
-                <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p onClick={this.handlePClick}>
-                            {name} {counter}
-                        </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            onClick={this.handleAClick}
-                        >
-                            Este é o link
-                        </a>
-                    </header>
-                </div>
-                <div>Oi</div>
-            </>
+            <div className="App">
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
         );
     }
 }
