f9e8722d07d5c069292dcb78075f33a382924ee8
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-10-07 22:38:56 -0300

    Introdução à testes com o Jest e Testing Library - Parte 3

--- project-1/src/components/Button/Button.spec.jsx
+++ project-1/src/components/Button/Button.spec.jsx
@@ -30,10 +30,21 @@ describe("<Button />", () => {
   });
 
   it("should be enabled when disabled is false", () => {
-    render(<Button text="Load more" disabled={false} />);
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
 
     const button = screen.getByRole("button", { name: /load more/i });
 
     expect(button).toBeEnabled();
+  });
+
+  it("should match snapshot", () => {
+    const fn = jest.fn();
+    const { container } = render(
+      <Button text="Load more" disabled={false} onClick={fn} />
+    );
+
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
