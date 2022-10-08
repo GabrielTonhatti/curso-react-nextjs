b6bf0e401457937f34782255e222eb5a952273e7
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-10-07 22:00:47 -0300

    Introdução à testes com o Jest e Testing Library - Parte 2

--- project-1/src/templates/Home/Home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Home } from "./index";
-
-test("renders Load more posts", () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/Load more posts/i);
-  expect(linkElement).toBeInTheDocument();
-});
