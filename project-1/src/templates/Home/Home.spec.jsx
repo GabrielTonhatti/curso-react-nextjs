e6e9246d359be42b5fe43509c97e4ac5e2cf91fb
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-11-10 09:10:13 -0300

    Testando os componentes renderizados pelo template <Home />

--- project-1/src/templates/Home/Home.spec.jsx
+++ project-1/src/templates/Home/Home.spec.jsx
@@ -5,7 +5,7 @@ import { render, screen, waitForElementToBeRemoved } from '@testing-library/reac
 import { Home } from '.';
 
 const handlers = [
-  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
     return res(
       ctx.json([
         {
@@ -21,40 +21,6 @@ const handlers = [
           title: 'title2',
           body: 'body2',
           url: 'img1.jpg',
-        },
-        {
-          userId: 3,
-          id: 3,
-          title: 'title3',
-          body: 'body3',
-          url: 'img3.jpg',
-        },
-      ]),
-    );
-  }),
-  rest.get('https://jsonplaceholder.typicode.com/photos', async (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          userId: 1,
-          id: 1,
-          title: 'title1',
-          body: 'body1',
-          url: 'img1.jpg',
-        },
-        {
-          userId: 2,
-          id: 2,
-          title: 'title2',
-          body: 'body2',
-          url: 'img1.jpg',
-        },
-        {
-          userId: 3,
-          id: 3,
-          title: 'title3',
-          body: 'body3',
-          url: 'img3.jpg',
         },
       ]),
     );
@@ -70,7 +36,18 @@ describe('<Home />', () => {
     render(<Home />);
     const noMorePosts = screen.getByText('Não existem posts =(');
 
+    expect.assertions(3);
+
     await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
   });
 
   afterEach(() => server.resetHandlers());
