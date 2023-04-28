c7582557ac174ac3a332135b467e429c36efb20c
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-04-28 20:07:32 -0300

    Inicia aula de useCallback

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,44 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import logo from './logo.svg';
+import PropsTypes from 'prop-types';
 
-const eventFn = () => {
-  console.log('h1 clicado');
+const Button = ({ incrementButton }) => <button onClick={() => incrementButton(10)}>+</button>;
+
+Button.propTypes = {
+  incrementButton: PropsTypes.func.isRequired,
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // componentDidUpdate = executa toda vez que o componente é atualizado
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-
-  // componentDidMount = executa uma única vez quando o componente é montado
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-    console.log('componentDidMount');
-
-    // componentWillUnmount =  (limpeza) executa quando o componente é desmontado
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, []);
-
-  // Com dependência = executa toda vez que a dependência é mudar
-  useEffect(() => {
-    console.log('C1: ', counter, 'C2: ', counter2);
-  }, [counter, counter2]);
+  const incrementCounter = (num) => setCounter(counter + num);
 
   return (
     <div className="App">
       <p>Teste 3</p>
-      <h1>
-        C 1: {counter} C 2: {counter2}
-      </h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+ (2)</button>
+      <h1>C 1: {counter}</h1>
+      <Button incrementButton={incrementCounter}>+</Button>
     </div>
   );
 }
