457903b01e7c6639dc76d82236e1ed00aeb5d608
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2023-05-18 18:00:52 -0300

    Criando rotas com componentes

--- project-2/src/index.jsx
+++ project-2/src/index.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './templates/App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </BrowserRouter>,
+);
