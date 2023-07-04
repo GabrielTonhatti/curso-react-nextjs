deee5d4597a86704d460a06905f48dada42cf825
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2023-07-04 19:14:13 -0300

    Rota 404: Rota pega tudo que não existe

---
+++ tutorial-react-route-dom/src/components/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react";
+import "./styles.css";
+
+const NotFound: () => ReactElement = (): ReactElement => {
+  return (
+    <div>
+      <h1>Page not Found.</h1>
+    </div>
+  );
+};
+
+export default NotFound;
