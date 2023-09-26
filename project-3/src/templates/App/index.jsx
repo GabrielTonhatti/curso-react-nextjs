e7455a150aef3f60a0aca2c9eb7f506f68a0b561
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-09-26 19:35:58 -0300

    Configurando o ThemeProvider do styled-components

--- project-3/src/templates/App/index.jsx
+++ project-3/src/templates/App/index.jsx
@@ -3,7 +3,7 @@ import * as Styled from './styles';
 const Home = () => {
   return (
     <div className="App">
-      <Styled.Wrapper>
+      <Styled.Wrapper background="blue">
         <h1>Hello</h1>
       </Styled.Wrapper>
     </div>
