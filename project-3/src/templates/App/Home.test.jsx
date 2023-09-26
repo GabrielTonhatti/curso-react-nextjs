a8b0b3e5118fcf42baffb0a879b7b7526eec055f
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-09-26 20:31:46 -0300

    Configurando o Jest e Jest-styled-components

---
+++ project-3/src/templates/App/Home.test.jsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react';
+import Home from '.';
+import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
+
+test('renders learn react link', () => {
+  const { debug } = renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', { name: 'Hello' }).parentElement;
+  debug(headingContainer);
+  expect(headingContainer).toHaveStyle({
+    background: theme.colors.secondaryBg,
+  });
+  expect(headingContainer).toHaveStyleRule('background', theme.colors.secondaryBg);
+  expect(headingContainer).toMatchSnapshot();
+});
